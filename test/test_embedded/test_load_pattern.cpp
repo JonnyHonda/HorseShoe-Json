962fe165c0589d6aa2c0a14710c74b6a19e56505
John <john@synergitech.co.uk>
2020-01-22 16:30:43 +0000

    Add embedded test unit folder

---
+++ test/test_embedded/test_load_pattern.cpp
@@ -0,0 +1,22 @@
+#include <unity.h>
+#include <string>
+#include "ArduinoJson.h"
+#include "patterngenerator.h"
+
+void test_function_load_json_pattern(void) {
+    PatternGenerator testPattern;
+    bool result = testPattern.load("./sd_card/shoerow1.json");
+
+    TEST_ASSERT_FALSE(result);
+    TEST_ASSERT_EQUAL_INT16(1000,testPattern.get_delay());
+    TEST_ASSERT_EQUAL_STRING("Chase Shoe By Row Up", testPattern.get_name().c_str());  
+    TEST_ASSERT_EQUAL_STRING("Turn on each row Shoes on in sequence up", testPattern.get_description().c_str());  
+}
+
+int main(int argc, char **argv) {
+    UNITY_BEGIN();
+    RUN_TEST(test_function_load_json_pattern);
+    UNITY_END();
+
+    return 0;
+}
